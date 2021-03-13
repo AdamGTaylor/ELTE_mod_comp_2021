e5af6d172f61417b6dab079b5ef2a3ccf57504fb
Szabo Adam Gergely <szabo.adam.g@gmail.com>
2021-03-13 16:15:35 +0100

    init /w hw1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# korszer2021
-Description
+# Modern Computer Methods in Physics I. (2020/2021/2)
+
+This is a repository dedicated to the course named above. This repository will contain the _homeworks_ wih their deadlines stated here, the project work and its documentation about its development. This readme file will contain the descriptions of major commits. For professionality, the binary files (.exe,.bin,...etc) will be ignore when committing. (.gitignore)
+
+## State of the project
+
+"""
+Soon.
+"""
+
+## Commits
+
+### 2020.03.13,16:10: Init
+> After some time, this repository started with a basic setup. Containes hw1 start.
---
+++ hw1/build/CMakeFiles/hw1-newton.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Adam/_ELTE_PHYS_MSC/2_second_semester/korszer/hw1/main.cpp" "D:/Adam/_ELTE_PHYS_MSC/2_second_semester/korszer/hw1/build/CMakeFiles/hw1-newton.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
