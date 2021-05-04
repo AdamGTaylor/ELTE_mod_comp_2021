dedbfdfa33e29e82e291ee4e8715f5236dfdd869
Szabó Ádám Gergely <szabo.adam.g@gmail.com>
2021-05-04 18:15:06 +0200

    2021/05/04:RK4.hpp simplified

--- project/odeint.hpp
+++ project/odeint.hpp
@@ -13,12 +13,9 @@
 template<typename X,typename DER, typename TAU> void RK4Step(std::vector<X>& x, TAU& tau, DER der){
     //this is fine like this, but the problem is that these could be more compact...
     vector<X> k1(x.size()), k2(x.size()), k3(x.size()), k4(x.size());
-    for(int i=0; i<x.size(), ++i){
-        k1[i] = tau * der(x[i]);
-        k2[i] = tau * der(x[i] + 0.5 * k1[i]);
-        k3[i] = tau * der(x[i] + 0.5 * k2[i]);
-        k4[i] = tau * der(x[i] + k3[i]);
-        x[i] += (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i])/6.0; 
-    }
-
+    k1 = tau * der(x);
+    k2 = tau * der(x + 0.5 * k1);
+    k3 = tau * der(x + 0.5 * k2);
+    k4 = tau * der(x + k3);
+    x += (k1 + 2 * k2 + 2 * k3 + k4)/6.0; 
 }
